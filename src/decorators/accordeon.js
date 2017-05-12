d19717280fc46f3327a47b59c4e66f8ad71cf5ef
Alexander Nikolskiy <vsol@bk.ru>
2017-05-12 20:41:23 +0300

    Task2 completed

--- src/decorators/accordeon.js
+++ src/decorators/accordeon.js
@@ -1,7 +1,7 @@
-import React, { Component as BasicComponent } from 'react';
+import React from 'react';
 
 //DecoratedComponent я называл чтоб легче понять было. Лучше выбирай более значущее название
-export default (Component) => class AccordeonDecorator extends BasicComponent {
+export default (Component) => class AccordeonDecorator extends React.Component {
     state = {
         //Не привязывайся к названиям сущностей, вся суть декораторов в универсальности. Сделай openItemId
         openItemId: null
