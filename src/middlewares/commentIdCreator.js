34006b014a17d261653d98bab0673a0666b82126
Roman <kaboom.ua@gmail.com>
2017-05-22 14:55:43 +0300

    Update commentIdCreator.js

--- src/middlewares/commentIdCreator.js
+++ src/middlewares/commentIdCreator.js
@@ -3,6 +3,7 @@ import { ADD_COMMENT } from '../constants'
 export default store => next => action => {
     const {type, payload} = action;
     switch (type) {
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - не лучшее решение
         case ADD_COMMENT:
             const { comments } = store.getState();
             action.payload.id = +Object.keys(comments).reduce((acc, id) => (+id > +acc ? id : acc)) + 1;
